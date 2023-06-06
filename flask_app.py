ed6e98c1e32eff96144dec62f029a31059ebfb13
Esammy <egwusamuel2015@gmail.com>
2023-06-06 12:32:09 +0100

    saveimages

--- flask_app.py
+++ flask_app.py
@@ -2,11 +2,18 @@ from flask import Flask, request, render_template
 import numpy as np
 import cv2
 from Similarity_check import load_model, resize_image, check_similarities
+import os
 
 
 app = Flask(__name__)
 
-
+def assure_path_exists(path):
+  try:
+      dir = os.path.dirname(path)
+      if not os.path.exists(dir):
+          os.makedirs(dir)
+  except:
+      print('Error has occured')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,10 +21,16 @@ def index():
         # Get the uploaded images
         image1 = request.files['image1']
         image2 = request.files['image2']
+        # print('the file name is:',image1.filename)
+        path = 'image/jpeg/'
+        assure_path_exists(path)
+        
+        image1.save(path)
+        image2.save(path)
 
         # Preprocess the images
-        img1 = resize_image(image1)
-        img2 = resize_image(image2)
+        img1 = resize_image(path+image1.filename)
+        img2 = resize_image(path+image2.filename)
 
         # Perform inference with the Siamese model (adjust as needed)
         similarity_score = check_similarities(img1, img2)
--- streamlit_app.py
+++ streamlit_app.py
@@ -12,23 +12,6 @@ import streamlit as st
 import numpy as np
 import cv2
 
-# # Load the Siamese model
-# model = tf.keras.models.load_model('siamese_model.h5')
-
-# # Define a function to predict the similarity between two images
-# def predict_similarity(image1, image2):
-#     # Convert the images to NumPy arrays
-#     image1 = np.array(image1)
-#     image2 = np.array(image2)
-
-#     # Resize the images to the same size
-#     image1 = cv2.resize(image1, (224, 224))
-#     image2 = cv2.resize(image2, (224, 224))
-
-#     # Predict the similarity between the two images
-#     similarity = model.predict([image1, image2])
-
-#     return similarity
 
 # Create a Streamlit app
 st.title('Siamese Image Similarity App')
@@ -57,35 +40,3 @@ if image1 and image2:
     st.image(image1, caption='Image 1')
     st.image(image2, caption='Image 2')
 
-
-
-######
-# # Load your Siamese network model
-# model = load_model()
-
-# # Define the Streamlit app
-# def main():
-#     st.title("Siamese Network App")
-#     st.write("Upload two images and get the similarity score!")
-
-#     # Allow users to upload images
-#     image1 = st.file_uploader("Upload Image 1", type=['jpg', 'jpeg', 'png'])
-#     image2 = st.file_uploader("Upload Image 2", type=['jpg', 'jpeg', 'png'])
-#     print(image1)
-
-#     if image1 and image2:
-#         # Display the uploaded images
-#         st.image([image1, image2], caption=["Image 1", "Image 2"], width=200)
-
-#         # Preprocess the images
-#         image_1 = resize_image(image1)
-#         image_2 = resize_image(image2)
-
-#         similarity_score = check_similarities(image_1, image_2)
-
-#         # Display the similarity score
-#         st.write("Similarity Score:", similarity_score)
-
-# # Run the app
-# if __name__ == '__main__':
-#     main()
